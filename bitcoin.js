3c6308c75e71d30b17c07ef355bd196e5699d80e
LMP506W <luxuryscents240@gmail.com>
2026-02-20 18:01:28 +0100

    Update bitcoin.js

--- bitcoin.js
+++ bitcoin.js
@@ -1,10 +1,2 @@
-const BTC_ADDRESS = "bc1qvmxrmyspaf8259xdgrg77pn504knf3sjal457k";
-
-function openBitcoin() {
-    document.getElementById("panel").innerHTML = `
-        <p>Send BTC to:</p>
-        <p>${BTC_ADDRESS}</p>
-        <p><a href="https://mempool.space/address/${BTC_ADDRESS}" target="_blank">View on mempool.space</a></p>
-        <p>You will receive nothing.</p>
-    `;
-}
+const BTC_ADDRESS="bc1qvmxrmyspaf8259xdgrg77pn504knf3sjal457k";
+function openBitcoin(){document.getElementById("panel").innerHTML=`<p>Send BTC to:</p><p>${BTC_ADDRESS}</p><p><a href="https://mempool.space/address/${BTC_ADDRESS}" target="_blank">View on mempool.space</a></p><p>You will receive nothing.</p>`;}
